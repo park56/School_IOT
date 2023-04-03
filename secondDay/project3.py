36ed48654c45dfdaa55ac30acfa1214c43f44f85
박성한 <92065248+park56@users.noreply.github.com>
2023-04-03 16:08:46 +0900

    first test

---
+++ secondDay/project3.py
@@ -0,0 +1,29 @@
+#-*- coding:utf-8 -*-
+import RPi.GPIO as GPIO			
+
+import time						
+
+led_pin1 = 23
+led_pin2 = 24
+button = 21
+
+GPIO.setwarnings(False)	
+
+GPIO.setmode(GPIO.BCM)	
+
+GPIO.setup(led_pin1, GPIO.OUT)
+GPIO.setup(led_pin2, GPIO.OUT)
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+	while GPIO.input(button) == 1:
+		GPIO.output(led_pin1, 1)
+		time.sleep(0.1)
+		GPIO.output(led_pin1, 0)
+		time.sleep(0.1)
+		GPIO.output(led_pin2, 1)
+		time.sleep(0.1)
+		GPIO.output(led_pin2, 0)
+		time.sleep(0.1)
+
+GPIO.cleanup()
